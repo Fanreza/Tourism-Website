0a34bbbd8aab61ec6568dc7d2e84aff6600b082d
Muhamad Jamil <57042204+Fanreza@users.noreply.github.com>
2021-03-10 13:09:03 +0700

    Add files via upload

---
+++ aset/php/header.php
@@ -0,0 +1,18 @@
+	
+
+	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+	<meta name="description" content="we are a platform for tourists who want to define their journey with a variety of experiences">
+	<meta name="keywords" content="GeoTourism Sentul,Wisata Sentul,Wisata Bogor,Tourism,Wisata Alam,GeoTourism">
+	<meta name="author" content="GeoTourism">
+	<meta property="og:title" content="GeoTourism Sentul"/>
+	<meta property="og:image" content="aset/image/logo-web.png">
+	<meta property="og:url" content="https://geotourism.rf.gd/" />
+	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences">
+
+	<link rel="shortcut icon" type="image/png" href="aset/image/logo-web.png" sizes="200x200">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
+	<!-- 	<link href="aset/fontawesome/css/fontawesome.css" rel="stylesheet">
+  	<link href="aset/fontawesome/css/brands.css" rel="stylesheet">
+  	<link href="aset/fontawesome/css/solid.css" rel="stylesheet"> -->
+  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
