02cbf34f798fca425bd38819e7a12322a6574c10
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-03-10 15:00:45 +0700

    Update header.php

--- aset/php/header.php
+++ aset/php/header.php
@@ -1,30 +1,23 @@
 	
 
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
-	<meta name="description" content="we are a platform for tourists who want to ">
+	<meta name="description" content="we are a platform for tourists who want to define their journey with a variety of experiences">
 	<meta name="keywords" content="GeoTourism Sentul,Wisata Sentul,Wisata Bogor,Tourism,Wisata Alam,GeoTourism">
 	<meta name="author" content="GeoTourism">
-	
-	<!-- Global Open Graph -->
+
+	<!-- Open Graph Meta Tags -->
 	<meta property="og:url" content="https://geotourism.herokuapp.com">
 	<meta property="og:type" content="website">
 	<meta property="og:title" content="GeoTourism Sentul">
-	<meta property="og:description" content="We are a platform for tourists global">
+	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
 	<meta property="og:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
 
-	<!-- Facebook Meta Tags -->
-	<meta property="og:url" content="https://geotourism.herokuapp.com">
-	<meta property="og:type" content="website">
-	<meta property="og:title" content="GeoTourism Sentul">
-	<meta property="og:description" content="We are a platform for tourists facebookl">
-	<meta property="og:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
-
-	<!-- Twitter Meta Tags -->
+	<!-- Open Graph Twitter Meta Tags -->
 	<meta name="twitter:card" content="summary_large_image">
 	<meta property="twitter:domain" content="geotourism.herokuapp.com">
 	<meta property="twitter:url" content="https://geotourism.herokuapp.com">
 	<meta name="twitter:title" content="GeoTourism Sentul">
-	<meta name="twitter:description" content="We are a platform for tourists twioter">
+	<meta name="twitter:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
 	<meta name="twitter:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
 
 	<!-- Link For CSS and ICON -->
