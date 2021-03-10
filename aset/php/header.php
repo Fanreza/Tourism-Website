80421f0999cb066969aaef4ef2db5e8a20f0bdf1
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-03-10 14:46:47 +0700

    Update header.php
    add a open graph tag for twitter and facebook

--- aset/php/header.php
+++ aset/php/header.php
@@ -5,14 +5,31 @@
 	<meta name="keywords" content="GeoTourism Sentul,Wisata Sentul,Wisata Bogor,Tourism,Wisata Alam,GeoTourism">
 	<meta name="author" content="GeoTourism">
 	<meta property="og:title" content="GeoTourism Sentul"/>
-	<meta property="og:image" content="aset/image/logo-web.png">
+	<meta property="og:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
 	<meta property="og:url" content="https://geotourism.rf.gd/" />
 	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences">
 
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://geotourism.herokuapp.com">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="GeoTourism Sentul">
+	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences">
+	<meta property="og:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta property="twitter:domain" content="geotourism.herokuapp.com">
+	<meta property="twitter:url" content="https://geotourism.herokuapp.com">
+	<meta name="twitter:title" content="GeoTourism Sentul">
+	<meta name="twitter:description" content="We are a platform for tourists who want to define their journey with a variety of experiences">
+	<meta name="twitter:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
+
+	<!-- Link For CSS and ICON -->
 	<link rel="shortcut icon" type="image/png" href="aset/image/logo-web.png" sizes="200x200">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
-	<!-- 	<link href="aset/fontawesome/css/fontawesome.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+	<!--<link href="aset/fontawesome/css/fontawesome.css" rel="stylesheet">
   	<link href="aset/fontawesome/css/brands.css" rel="stylesheet">
   	<link href="aset/fontawesome/css/solid.css" rel="stylesheet"> -->
-  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+  	
