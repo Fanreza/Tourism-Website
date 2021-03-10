fa9daf37b6fa5914ed46d29baad74a8465274d4f
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-03-10 18:47:36 +0700

    Update header.php
    add cdn css bootstrap

--- aset/php/header.php
+++ aset/php/header.php
@@ -26,7 +26,7 @@
 
 	<!-- Link For CSS and ICON -->
 	<link rel="shortcut icon" type="image/png" href="aset/image/logo-web.png" sizes="200x200">
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
