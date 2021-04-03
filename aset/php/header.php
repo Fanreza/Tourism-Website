8219907a10c9d74fe6801b9d69e9c3dacb5c0b9f
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-04-04 01:54:13 +0700

    Update header.php
    add two open graph for develope because my free hosting cannot show a open graph,so i use heroku for testing a open graph

--- aset/php/header.php
+++ aset/php/header.php
@@ -1,35 +1,51 @@
 	
-
+	
+	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
 	<meta name="description" content="we are a platform for tourists who want to define their journey with a variety of experiences">
 	<meta name="keywords" content="GeoTourism Sentul,Wisata Sentul,Wisata Bogor,Tourism,Wisata Alam,GeoTourism">
 	<meta name="author" content="GeoTourism">
 
 	<!-- Open Graph Meta Tags -->
+	<!-- For Deploy-->
+	<!-- <meta property="og:url" content="https://geotourism.rf.gd">
+	<meta property="og:type" content="article">
+	<meta property="og:title" content="GeoTourism Sentul">
+	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
+	<meta property="og:image" content="https://https://geotourism.rf.gd/aset/image/logo-web.png"> -->
+
+	<!-- For Develope -->
 	<meta property="og:url" content="https://geotourism.herokuapp.com">
-	<meta property="og:type" content="website">
+	<meta property="og:type" content="article">
 	<meta property="og:title" content="GeoTourism Sentul">
 	<meta property="og:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
 	<meta property="og:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
-	<meta property="og:image:width" content="300">
-	<meta property="og:image:height" content="300">
 
 	<!-- Open Graph Twitter Meta Tags -->
-	<meta name="twitter:card" content="summary_large_image">
-	<meta property="twitter:domain" content="geotourism.herokuapp.com">
+	<!-- For Deploy -->
+	<!-- <meta name="twitter:card" content="summary">
+	<meta property="twitter:domain" content="https://geotourism.rf.gd">
+	<meta property="twitter:url" content="https://https://geotourism.rf.gd">
+	<meta name="twitter:title" content="GeoTourism Sentul">
+	<meta name="twitter:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
+	<meta name="twitter:image" content="https://https://geotourism.rf.gd/aset/image/logo-web.png"> -->
+
+	<!-- For Develope -->
+	<meta name="twitter:card" content="summary">
+	<meta property="twitter:domain" content="https://geotourism.herokuapp.com">
 	<meta property="twitter:url" content="https://geotourism.herokuapp.com">
 	<meta name="twitter:title" content="GeoTourism Sentul">
 	<meta name="twitter:description" content="We are a platform for tourists who want to define their journey with a variety of experiences, we allow travelers to determine their own accommodation needed.">
 	<meta name="twitter:image" content="https://geotourism.herokuapp.com/aset/image/logo-web.png">
-	<meta property="og:image:width" content="300">
-	<meta property="og:image:height" content="300">
+
 
 	<!-- Link For CSS and ICON -->
 	<link rel="shortcut icon" type="image/png" href="aset/image/logo-web.png" sizes="200x200">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
+	<!-- Fontawesome -->
 	<!--<link href="aset/fontawesome/css/fontawesome.css" rel="stylesheet">
   	<link href="aset/fontawesome/css/brands.css" rel="stylesheet">
   	<link href="aset/fontawesome/css/solid.css" rel="stylesheet"> -->
