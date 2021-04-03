1b4539f14b13ff55f216d9b5558753459f08803e
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-04-04 01:58:07 +0700

    Update footer.php
    putting all footer section to footer.php

--- aset/php/footer.php
+++ aset/php/footer.php
@@ -1,6 +1,33 @@
+	<!-- Footer -->
+	<div class="footer" id="contact">
+		<div class="image-brand">
+			<img src="aset/image/logo-web.png">
+			<p>We provide the best service, work quickly, accurately, and professionally. <span>what you want is what you get</span></p>
+		</div>
 
+		<div class="support">
+			<h3>Customer Support </h3>
+			<i class="bi bi-geo-alt-fill"> Sentul Nirwana</i>
+			<i class="bi bi-telephone-fill"> 08111036811</i>
+			<a href="https://wa.me/628111036811" target="_blank"><i class="bi bi-whatsapp"> 08111036811</i></a>
+			<i class="bi bi-envelope-fill"> hikingsentul@gmail.com</i>
+			<a href=""><i class="bi bi-question-circle-fill"> FAQ</i></a>
+		</div>
 
+		<div class="social">
+			<h3>Social Media</h3>
+			<div class="image">
+				<a href="https://www.instagram.com/geotourismsentul/" target="_blank"><i class="bi bi-instagram"></i></a>
+			</div>	
+		</div>
+	</div>
 
+	<!-- COPYRIGHT -->
+	<div class="copyright">
+		<p>&copy; 2020 - All Right Reserved. Designed and Developed by <span><a href="">Geo Tourism Sentul</a></span></p>
+	</div>
+
+	<!-- Javasript -->
 	<!-- For Develope -->
 	<!-- <script src="bootstrap/js/jquery-3.4.1.slim.min.js"></script>
 	<script src="bootstrap/js/popper.min.js"></script>
@@ -10,3 +37,13 @@
 	<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+	 $(window).scroll(function(){
+       $('nav').toggleClass('scroll', $(this).scrollTop() > 10);
+       });
+
+	 $(window).scroll(function(){
+       $('ul').toggleClass('scroll', $(this).scrollTop() > 10);
+       });
+
+	</script>
