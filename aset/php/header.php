8c95a5fe7e69dd73fe2909e752beab7d5bec4144
Muhamad Jamil <rezaramdhani461@gmail.com>
2021-04-13 21:25:13 +0700

    Add files via upload

--- aset/php/header.php
+++ aset/php/header.php
@@ -46,11 +46,11 @@
 	<!-- Link For CSS and ICON -->
 	<link rel="shortcut icon" type="image/png" href="aset/image/logo-web.png" sizes="144x144">
 	<!-- For Deploy -->
-	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"> -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="aset/css/style.css">
 
 	<!-- For Develope -->
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css"> -->
 
 	<!-- This code interferes with the deployment process, please delete it after develope, then return it after deploy  -->
 
